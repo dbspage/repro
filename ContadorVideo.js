096747d625eda9043113d0588c7bb194c4512b11
malcomelde001 <74287959+malcomelde001@users.noreply.github.com>
2020-11-11 01:16:19 -0600

    Create ContadorVideo.js

---
+++ ContadorVideo.js
@@ -0,0 +1,8 @@
+var sc_project=11473394;
+var sc_invisible=1;
+var sc_security="4621dfdd";
+var scJsHost = (("https:" == document.location.protocol) ?
+"https://secure." : "http://www.");
+document.write("<sc"+"ript type='text/javascript' src='" +
+scJsHost+
+"statcounter.com/counter/counter.js'></"+"script> ");
